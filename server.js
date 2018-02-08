64a312b71053847938cc3e3bf1cde65f516d87d0
Kyle L. Jensen <kljensen@gmail.com>
2018-02-08 22:08:13 +0000

    Finish chat demo

--- server.js
+++ server.js
@@ -2,23 +2,45 @@ const express = require('express')
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const db = new sqlite3.Database(process.env.DATABASE);
+const path = require('path');
+var bodyParser = require('body-parser');
 
+// Use this to parse the body of post requests
+app.use(bodyParser.json())
+
+// Use this to server static files from the 'static' directory
+app.use('/static', express.static('static'))
+
+// Our homepage---just send the index.html file
 app.get('/', (req, res) => {
-  let body = "<html><body><ul>";
-  db.all("SELECT * FROM diary order by date", function(err, rows) {
-   for(var i=0; i<rows.length; i++){
-    const row = rows[i];
-    body = `${body}\n<li>${row.date} -- ${row.body}</li>`;
-   }
-   body += "</ul></body></html>";
-   res.send(body);
-  });
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Our API for getting chats
+app.get('/api/chats', (req, res) => {
+	getChats(req.query.latest || 0, function(rows){
+		res.send(rows);
+	});
+});
+
+// Our API for posting new chats
+app.post('/api/chats', (req, res) => {
+	const chatBody = req.body.body;
+	db.all('INSERT INTO chats (body) VALUES (?)', chatBody, function(err, rows){
+		// Return a 500 status if there was an error, otherwise success status
+		res.send(err ? 500 : 200);
+	});
+});
+
+function getChats(latestID, cb){
+	db.all('SELECT rowid,body,date FROM chats WHERE rowid > (?)', latestID, function(err, rows){
+		cb(rows);
+	});
+}
 
 
 const create_table = `
-CREATE TABLE IF NOT EXISTS diary (
+CREATE TABLE IF NOT EXISTS chats (
   date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   body TEXT
 )
@@ -27,6 +49,7 @@ CREATE TABLE IF NOT EXISTS diary (
 
 db.serialize(function() {
   db.run(create_table);
-  app.listen(9000, () => console.log('Example app listening on port 3000!'))
+	const port = process.env.PORT;
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 });
 
